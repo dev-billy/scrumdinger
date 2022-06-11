9374036ccda03116118adbf8fd3753be7ce00a85
Billy <billypatrick767@gmail.com>
2022-06-11 17:50:30 +0300

    added: list view for scrums

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -6,18 +6,28 @@
 //
 
 import Foundation
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    let id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme ){
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
+    
 }
 
 extension DailyScrum {
     static let sampleData:[DailyScrum] =
     [
         DailyScrum(title: "Design", attendees: ["Filly", "Jane", "Helen", "Doe"], lengthInMinutes: 10, theme: .yellow),
-        DailyScrum(title: "Development", attendees: ["Grant", "Branson", "Allen"], lengthInMinutes: 5, theme: .orange),
-        DailyScrum(title: "Implementation", attendees: ["James", "Victoria"], lengthInMinutes: 20, theme: .buttercup)
+        DailyScrum(title: "Development", attendees: ["Grant", "Branson", "Allen"], lengthInMinutes: 5, theme: .navy),
+        DailyScrum(title: "Implementation", attendees: ["James", "Victoria"], lengthInMinutes: 20, theme: .poppy)
     ]
 }

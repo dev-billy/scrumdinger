84171e3bcb68b9220825c751d75ed511cdd2aef3
Billy <billypatrick767@gmail.com>
2022-06-11 21:31:59 +0300

    added: detail view with details

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,50 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Billy Patrick on 11/06/2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        List{
+            Section(header: Text("Meeting Info")){
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack{
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) Minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack{
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text("\(scrum.theme.name)")
+                        .padding(5)
+                        .background(scrum.theme.mainColor)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .cornerRadius(5)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendees")){
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}

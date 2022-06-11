ad49544ca374cab8308b50f3febda222ba3d3da9
Billy <billypatrick767@gmail.com>
2022-06-11 12:08:57 +0300

    first commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Billy Patrick on 11/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
